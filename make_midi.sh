4f63a72595329d679c561b17fd071567e17bca36
Slatina <slatina.church@gmail.com>
2020-10-22 20:12:08 +0300

    Add MuseScore MIDI source files and a script to generate the actual MIDI files

---
+++ make_midi.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+INTROS_SOURCE_DIR="./MIDIs/Intros/Source"
+INTROS_MIDI_OUTPUT_DIR="./MIDIs/Intros"
+SONGS_SOURCE_DIR="./MIDIs/Songs/Source"
+SONGS_MIDI_OUTPUT_DIR="./MIDIs/Songs"
+MUSESCORE_EXECUTABLE="$1"
+
+show_help() {
+cat <<'EOF'
+Usage: make_midi.sh <MUSESCORE_EXECUTABLE>
+Generate MIDI files from the MSCZ source files.
+
+Dependencies:
+- MuseScore 3.5+
+EOF
+}
+
+check_dependencies() {
+    if [ ! -f "$MUSESCORE_EXECUTABLE" ]; then
+        echo "Could not find MuseScore executable. Aborting script."
+        show_help
+        exit 1
+    fi
+}
+
+generate_intros_midi() {
+    for file in "$INTROS_SOURCE_DIR/"*.mscz; do
+        if [ ! -f "$file" ]; then
+            echo "Missing file: $file"
+            continue
+        fi
+
+        "$MUSESCORE_EXECUTABLE" "$file" -o "$INTROS_MIDI_OUTPUT_DIR/$(basename "${file%.mscz}").mid"
+    done
+}
+
+generate_songs_midi() {
+    for file in "$SONGS_SOURCE_DIR/"*.mscz; do
+        if [ ! -f "$file" ]; then
+            echo "Missing file: $file"
+            continue
+        fi
+
+        "$MUSESCORE_EXECUTABLE" "$file" -o "$SONGS_MIDI_OUTPUT_DIR/$(basename "${file%.mscz}").mid"
+    done
+}
+
+main() {
+    check_dependencies
+    generate_intros_midi
+    generate_songs_midi
+}
+
+main
