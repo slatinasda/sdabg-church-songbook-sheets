6fcb3fdf50c03b160e1e575ca273555fa4e0611c
Slatina <slatina.church@gmail.com>
2026-04-08 21:26:31 +0300

    Reference Open Sans fonts from the project directly

--- make_cover_page.sh
+++ make_cover_page.sh
@@ -2,25 +2,12 @@
 
 COVER_PAGE_DIR="./Cover"
 FILE_NAME="cover.pdf"
-SYSTEM_FONTS_DIR="$HOME/.local/share/fonts"
 
 
 check_dependencies() {
     if [ "$(command -v xelatex)" = "" ]; then
         echo "LaTeX is not installed on this system. Aborting script."
         exit 1
-    fi
-}
-
-install_required_font() {
-    if [ "$(fc-list opensans)" = "" ]; then
-        # Open Sans is not installed. Download and install
-        wget -c --content-disposition 'https://fonts.google.com/download?family=Open%20Sans' -P "$COVER_PAGE_DIR"
-        mkdir -p "$SYSTEM_FONTS_DIR"
-        unzip -j "$COVER_PAGE_DIR/Open_Sans.zip" -d "$SYSTEM_FONTS_DIR" "*.ttf"
-
-        # Remove the fonts which have "wght" in their name. They make the Latex script fail on Github CI
-        ls "$SYSTEM_FONTS_DIR"/*.ttf | grep ',wght.ttf' | xargs rm
     fi
 }
 
@@ -31,5 +18,4 @@ generate_cover_page() {
 }
 
 check_dependencies
-install_required_font
 generate_cover_page
